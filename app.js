0d2f37836a16b79118d64e9e7e2dcab66b589531
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-14 16:22:03 +0500

    for git commit -m for
    git commit -m for

---
+++ app.js
@@ -0,0 +1,44 @@
+// 1- masala
+
+// let a = +prompt(sonni kiriting);
+
+// let result = 0;
+// for ( i=0; i<=a; i++) {
+//     if (i % 3 == 0 || i % 7 == 0) {
+//         result = result += 1;
+//     }
+// }
+// console.log(result);
+
+
+// 2 - masala
+
+// let a = +prompt("sonni kiriting");
+
+// let result = 0;
+// for ( i=1; i=a; a--)
+// {
+//     if (a % 2 == 0) {
+//         result += a;
+//     }
+// }
+// console.log(result);
+
+
+// 3 - masala 
+
+// let a = +prompt("sonni kiriting");
+
+// let result = 0;
+
+// for (i = 0; i <= a; i++) {
+//     if (i % 5 == 0 || i % 7 == 0) {
+//         continue
+//     }
+//     result = result += i;
+// }
+
+// console.log(result);
+
+
+// 4 - masala
